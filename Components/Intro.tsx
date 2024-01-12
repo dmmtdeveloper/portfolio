a66f81b3ff39d633d0331b50102688d2e742bd56
David Martínez Tapia <dmmtapia.ux@gmail.com>
2024-01-11 23:54:37 -0300

    button contact cv

--- Components/Intro.tsx
+++ Components/Intro.tsx
@@ -2,6 +2,11 @@
 import Image from "next/image";
 import React from "react";
 import { animate, motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import clsx from "clsx";
+import { FaGithubSquare } from "react-icons/fa";
 
 export const Intro = () => {
   return (
@@ -51,6 +56,113 @@ export const Intro = () => {
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.p>
+
+      <motion.div
+        className={clsx(
+          "flex",
+          "flex-col",
+          "items-center",
+          "justify-center",
+          "gap-6",
+          /*responsive*/
+          "sm:flex-row"
+        )}
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        {/* ----Button Contact---- */}
+        <Link
+          href="#contact"
+          className={clsx(
+            "group",
+            "bg-gray-900",
+            "text-white",
+            "px-7 py-3",
+            "flex",
+            "items-center",
+            "gap-2",
+            "rounded-full",
+            "outline-none",
+            "focus:scale-110",
+            "hover:scale-110",
+            "hover:bg-gray-950",
+            "active:scale-105",
+            "transition duration-300"
+          )}
+        >
+          Contact me here{" "}
+          <BsArrowRight
+            className={clsx(
+              "opacity-70",
+              "group-hover:translate-x-1",
+              "transition duration-300"
+            )}
+          />
+        </Link>
+
+        {/* ----Button CV---- */}
+        <a
+          className={clsx(
+            "group",
+            "cursor-pointer",
+            "bg-white",
+            "text-gray-900",
+            "px-7 py-3",
+            "flex",
+            "items-center",
+            "gap-2",
+            "rounded-full",
+            "outline-none",
+            "focus:scale-110",
+            "hover:scale-110",
+            "active:scale-105",
+            "transition duration-300"
+          )}
+        >
+          Download CV{" "}
+          <HiDownload
+            className={clsx(
+              "opacity-60",
+              "group-hover:translate-y-1",
+              "transition duration-300"
+            )}
+          />{" "}
+        </a>
+
+        {/* ----Button Linkedin---- */}
+        <a
+          className={clsx(
+            "bg-white",
+            "text-gray-700",
+            "p-4",
+            "flex",
+            "items-center",
+            "gap-2",
+            "rounded-full"
+          )}
+        >
+          <BsLinkedin />
+        </a>
+
+        {/* ----Button GitHub---- */}
+        <a
+          className={clsx(
+            "bg-white",
+            "text-gray-700",
+            "p-4",
+            "flex",
+            "items-center",
+            "gap-2",
+            "rounded-full",
+            "text-[1.2rem]"
+          )}
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 };
